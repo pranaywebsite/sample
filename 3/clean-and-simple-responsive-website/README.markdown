2bcdd46963f5bd9d0fd350ae69b4e6fd77619336
pranaywebsite <65412903+pranaywebsite@users.noreply.github.com>
2020-07-01 01:36:50 +0530

    Add files via upload

---
+++ 3/clean-and-simple-responsive-website/README.markdown
@@ -0,0 +1,5 @@
+# Clean and simple responsive website
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/hoshiyar/pen/wvMqgwy](https://codepen.io/hoshiyar/pen/wvMqgwy).
+
+
---
+++ 3/navjs.js
@@ -0,0 +1,31 @@
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-60px";
+  }
+}
+/*----------------------------------------------------*/
+/* Smooth Scrolling
+------------------------------------------------------ */
+
+jQuery(document).ready(function($) {
+
+   $('.smoothscroll').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 800, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+  
+});
+
