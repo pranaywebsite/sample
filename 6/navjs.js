d33d9672a6b8ec8c1f0a629ee3913ef6262cc8ae
pranaywebsite <65412903+pranaywebsite@users.noreply.github.com>
2020-10-01 14:44:53 +0530

    Add files via upload

--- 6/navjs.js
+++ 6/navjs.js
@@ -2,30 +2,84 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-70px";
-  }
+	if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
+		document.getElementById("navbar").style.top = "0";
+	} else {
+		document.getElementById("navbar").style.top = "-70px";
+	}
 }
 /*----------------------------------------------------*/
-/* Smooth Scrolling
------------------------------------------------------- */
+	/* Smooth Scrolling
+	------------------------------------------------------ */
 
-jQuery(document).ready(function($) {
+	jQuery(document).ready(function($) {
 
-   $('.smoothscroll').on('click',function (e) {
-	    e.preventDefault();
+		$('.smoothscroll').on('click',function (e) {
+			e.preventDefault();
 
-	    var target = this.hash,
-	    $target = $(target);
+			var target = this.hash,
+			$target = $(target);
 
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 800, 'swing', function () {
-	        window.location.hash = target;
-	    });
+			$('html, body').stop().animate({
+				'scrollTop': $target.offset().top
+			}, 800, 'swing', function () {
+				window.location.hash = target;
+			});
+		});
+		
 	});
-  
-});
 
+
+
+
+
+$(function(){
+  var prevScroll = 0,
+      curDir = 'down',
+      prevDir = 'up';
+  
+  $(window).scroll(function(){
+    	if($(this).scrollTop() >= prevScroll){
+          curDir = 'down';
+          if(curDir != prevDir){
+          $('.navbar-custom').stop();
+    	  	$('.navbar-custom').animate({ bottom: '-50px' }, 300);
+          prevDir = curDir;
+          }
+      } else {
+          curDir = 'up';
+          if(curDir != prevDir){
+          $('.navbar-custom').stop();
+          $('.navbar-custom').animate({ bottom: '0px' }, 300);
+          prevDir = curDir;
+          }
+      }
+      prevScroll = $(this).scrollTop();
+  });
+})	
+
+
+$(function(){
+  var prevScroll = 0,
+      curDir = 'down',
+      prevDir = 'up';
+  
+  $(window).scroll(function(){
+    	if($(this).scrollTop() >= prevScroll){
+          curDir = 'down';
+          if(curDir != prevDir){
+          $('.my').stop();
+    	  	$('.my').animate({ bottom: '-50px' }, 300);
+          prevDir = curDir;
+          }
+      } else {
+          curDir = 'up';
+          if(curDir != prevDir){
+          $('.my').stop();
+          $('.my').animate({ bottom: '0px' }, 300);
+          prevDir = curDir;
+          }
+      }
+      prevScroll = $(this).scrollTop();
+  });
+})	
