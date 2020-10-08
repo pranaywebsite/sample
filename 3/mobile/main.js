2cc4e184983c1cf657a086bb242b3134047df83d
pranaywebsite <65412903+pranaywebsite@users.noreply.github.com>
2020-10-08 16:21:50 +0530

    Add files via upload

--- 3/mobile/main.js
+++ 3/mobile/main.js
@@ -1,7 +1,7 @@
 
 const typedTextSpan=document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
-const textArray=["Pranay Patel.","Knowledge.","A Journey.", "Truth.","A Human Being."];
+const textArray=["Pranay Patel.","Knowledge.","A Journey.", "Truth."];
 const typingDelay=200;
 const erasingDelay=100;
 const newTextDelay=2000;
